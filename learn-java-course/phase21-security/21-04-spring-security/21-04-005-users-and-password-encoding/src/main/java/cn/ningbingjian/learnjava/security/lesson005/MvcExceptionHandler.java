b7e8417a9b9672f224e9d2878715b8b562c9c53f
ningbingjian <ningbingjian@126.com>
2026-09-07 22:36:12 +0800

    docs(security): complete lesson 005 users and password encoding (#23)

---
+++ learn-java-course/phase21-security/21-04-spring-security/21-04-005-users-and-password-encoding/src/main/java/cn/ningbingjian/learnjava/security/lesson005/MvcExceptionHandler.java
@@ -0,0 +1,19 @@
+package cn.ningbingjian.learnjava.security.lesson005;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MvcExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(MvcExceptionHandler.class);
+
+    @ExceptionHandler(UnsupportedLanguageException.class)
+    ResponseEntity<ApiError> unsupportedLanguage() {
+        log.info("MVC_ERROR_HANDLED");
+        return ResponseEntity.badRequest().body(
+                new ApiError("UNSUPPORTED_LANGUAGE", "lang只支持en或zh，请修改参数后重试。"));
+    }
+}
